9610ccdc018e35c5dee432ea09159575b454e047
rajasekark54 <rajasekark54@gmail.com>
2022-07-05 13:39:03 +0530

    stack - infix to prefix conversion done with test cases

---
+++ src/3_Stack/4_INfixToPrefix.test.js
@@ -0,0 +1,132 @@
+const SYMBOLS = {
+  AT: '@',
+  OPEN_BRACKET: '(',
+  CLOSE_BRACKET: ')',
+  PLUS: '+',
+  MINUS: '-',
+  MULTIPLE: '*',
+  DIVID: '/',
+  POWER: '^',
+};
+
+class Stack {
+  constructor() {
+    this.list = [];
+    this.top = -1;
+  }
+
+  push(val) {
+    this.list[++this.top] = val;
+  }
+
+  pop() {
+    if (this.top > -1) {
+      return this.list[this.top--];
+    }
+    return 'Underflow';
+  }
+
+  peek() {
+    if (this.top > -1) {
+      return this.list[this.top];
+    }
+
+    return 'Empty List';
+  }
+}
+
+class InfixToPreFix extends Stack {
+  constructor() {
+    super();
+    this.postfix = [];
+  }
+
+  isOperator(elm) {
+    return Object.values(SYMBOLS).includes(elm);
+  }
+
+  precedence(elm) {
+    if (
+      SYMBOLS.AT === elm ||
+      SYMBOLS.OPEN_BRACKET === elm ||
+      SYMBOLS.CLOSE_BRACKET === elm
+    ) {
+      return 1;
+    } else if (SYMBOLS.PLUS === elm || SYMBOLS.MINUS === elm) {
+      return 2;
+    } else if (SYMBOLS.MULTIPLE === elm || SYMBOLS.DIVID === elm) {
+      return 3;
+    } else if (SYMBOLS.POWER === elm) {
+      return 4;
+    }
+
+    return 0;
+  }
+
+  conversion(str) {
+    let infix = str.trim().split('');
+    let infixReverseList = infix.reverse();
+
+    for (const elm of infixReverseList) {
+      if (this.isOperator(elm)) {
+        if (elm === SYMBOLS.CLOSE_BRACKET) {
+          this.push(elm);
+        } else if (elm === SYMBOLS.OPEN_BRACKET) {
+          while (this.top > -1 && this.peek() !== SYMBOLS.CLOSE_BRACKET) {
+            this.postfix.push(this.pop());
+          }
+          this.pop();
+        } else {
+          if (this.precedence(elm) > this.precedence(this.peek())) {
+            this.push(elm);
+          } else {
+            while (
+              this.top > -1 &&
+              this.precedence(elm) < this.precedence(this.peek())
+            ) {
+              this.postfix.push(this.pop());
+            }
+
+            this.push(elm);
+          }
+        }
+      } else {
+        this.postfix.push(elm);
+      }
+    }
+
+    while (this.top > -1) {
+      this.postfix.push(this.pop());
+    }
+
+    let revertRevers = this.postfix.reverse();
+    return revertRevers.join('');
+  }
+}
+describe('InfixToPostFix', () => {
+  let infToPos;
+
+  beforeEach(() => {
+    infToPos = new InfixToPreFix();
+  });
+
+  test('1', () => {
+    let res1 = infToPos.conversion('A+B*C+D');
+    expect(res1).toEqual('++A*BCD');
+  });
+
+  test('2', () => {
+    let res2 = infToPos.conversion('((a+b)*c)');
+    expect(res2).toEqual('*+abc');
+  });
+
+  test('3', () => {
+    let res3 = infToPos.conversion('a+b');
+    expect(res3).toEqual('+ab');
+  });
+
+  test('4', () => {
+    let res4 = infToPos.conversion('K+L-M*N+(O^P)*W/U/V*T+Q');
+    expect(res4).toEqual('++-+KL*MN*//*^OPWUVTQ');
+  });
+});
